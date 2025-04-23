c679fa41d6b00c3ea58c27ad4da4bfc34cacd014
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-04-23 15:31:10 +0200

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+  <>
+  <h1 className="text-4xl font-bold text-red-100">Hello</h1>
+  </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // تأكد من إضافة الملفات المناسبة هنا
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
