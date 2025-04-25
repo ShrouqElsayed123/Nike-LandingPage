fb8a808bd144ae6e5d19696b987c381dbfc7f5fa
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-04-25 03:07:44 -0700

    create components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import './index.css'
+import CustomersReviews from './Sections/CustomersReviews'
+import Footer from './Sections/Footer'
+import Hero from './Sections/Hero'
+import Nav from './Components/Nav'
+import PopularProducts from './Sections/PopularProducts'
+import Services from './Sections/Services'
+import SpecialOffers from './Sections/SpecialOffers'
+import Subscribe from './Sections/Subscribe'
+import SuperQuality from './Sections/SuperQuality'
 
 function App() {
 
   return (
   <>
-  <h1 className="text-4xl font-bold text-red-100">Hello</h1>
+<main className='relative'>
+  <Nav /> 
+  <section className='xl:padding-1 wide:padding-r padding-b'>
+   <Hero />
+  </section>
+  <section className='padding'> <PopularProducts /></section>
+  <section className='padding'> <SuperQuality /></section>
+  <section className='padding-x py-10'> <Services /></section>
+  <section className='padding'> <SpecialOffers /></section>
+  <section className='padding bg-pale-blue'> <CustomersReviews /></section>
+  <section className='padding-x sm:py-32 py-16 w-full'> <Subscribe /></section>
+  <section className='padding-x padding-t pb-8 bg-black'> <Footer /></section>
+</main>
   </>
   )
 }
---
+++ src/Sections/SpecialOffers.jsx
@@ -0,0 +1,9 @@
+
+export default function SpecialOffers() {
+  return (
+   <>
+   
+   
+   </>
+  )
+}
